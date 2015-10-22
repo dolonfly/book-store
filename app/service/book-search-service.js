5618389496610006b8f74353e2628c3ae8db772c
dll <kaixin890312@163.com>
2015-10-22 20:40:57 +0800

    only can search one book by isbn

--- app/service/book-search-service.js
+++ app/service/book-search-service.js
@@ -3,10 +3,13 @@
  */
 "use strict"
 var Book = require("../models/book-model");
+var isbnSuite = require('../utils/isbnSuite');
 
-function findByIsbn(isbns, callback) {
-    var isbnArray = isbns.split(",");
-    Book.find({isbn13: {$in: isbnArray}}).exec(callback);
+function findByIsbn(isbn, callback) {
+    if (isbn.length == 10) {
+        isbn = isbnSuite.convert.isbn10to13(isbn);
+    }
+    Book.find({isbn13: isbn}).exec(callback);
 }
 
 module.exports = {
