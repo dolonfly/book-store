2e223e2f2670626c03cfd0ae0130de963732fafc
dll <kaixin890312@163.com>
2016-01-02 18:50:20 +0800

    when a isbn is not a valide isbn ,not to search it

--- app/controller/isbn.js
+++ app/controller/isbn.js
@@ -6,6 +6,10 @@ var bookSearch = require("../service/book-search-service");
 
 function index(req, res, next) {
     var isbn = req.params.isbn;
+    if (!isbn || (isbn.length !== 10 && isbn.length !== 13)) {
+        res.render('pages/book-information', {book: null});
+        return;
+    }
 
     bookSearch.findByIsbn2(isbn, function (err, book) {
         if (err) {
